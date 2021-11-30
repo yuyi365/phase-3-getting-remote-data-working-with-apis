a9e0fefd93c4d7ac5d12820757be833702085137
Yuyi <yuyi365@gmail.com>
2021-11-30 16:02:04 -0500

    commit tree 20c7686e149a2ecbc986569c9d9abcf1c108f7dd

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,10 +1,9 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
-class GetPrograms
 
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+class GetPrograms
+  URL = 'http://data.cityofnewyork.us/resource/uvks-tn5n.json'
 
   def get_programs
     uri = URI.parse(URL)
@@ -12,7 +11,15 @@ class GetPrograms
     response.body
   end
 
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs = JSON.parse(self.get_programs)
+    programs.collect { |program| program['agency'] }
+  end
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq
--- nyc_api.rb
+++ nyc_api.rb
@@ -1,10 +1,9 @@
 require 'net/http'
- require 'open-uri'
- require 'json'
- 
- class GetPrograms
+require 'open-uri'
+require 'json'
 
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+class GetPrograms
+  URL = 'http://data.cityofnewyork.us/resource/uvks-tn5n.json'
 
   def get_programs
     uri = URI.parse(URL)
@@ -13,13 +12,14 @@ require 'net/http'
   end
 
   def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
     programs = JSON.parse(self.get_programs)
-    programs.collect do |program|
-      program["agency"]
-    end
+    programs.collect { |program| program['agency'] }
   end
-
 end
 
- programs = GetPrograms.new.get_programs
- puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq
